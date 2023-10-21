329a4e1620c4c808bb8cf15b5c0a0bbb18982988
skullteek23 <pgoel681@gmail.com>
2023-10-21 12:14:34 +0530

    added coming soon feature

--- src/app/rewards/rewards.module.ts
+++ src/app/rewards/rewards.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RewardsComponent } from './rewards.component';
 import { RouterModule } from '@angular/router';
+import { PlaceholderModule } from '@app/shared-modules/placeholder/placeholder.module';
 
 @NgModule({
   declarations: [RewardsComponent],
   imports: [
     CommonModule,
+    PlaceholderModule,
     RouterModule.forChild([{ path: '', component: RewardsComponent }]),
   ],
 })
-export class RewardsModule {}
+export class RewardsModule { }
