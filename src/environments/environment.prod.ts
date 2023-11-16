d8ec027401dfff2cfde39da4c9add7adbe713033
skullteek23 <pgoel681@gmail.com>
2023-11-16 12:37:55 +0530

    updated prod creds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,20 @@
 export const environment = {
   firebase: {
-    projectId: 'futball-platform-prod',
-    appId: '1:539449615245:web:380fc44fa65e4f793953c4',
-    databaseURL:
-      'https://futball-platform-prod-default-rtdb.asia-southeast1.firebasedatabase.app',
-    storageBucket: 'futball-platform-prod.appspot.com',
+    projectId: "futball-platform-prod",
+    appId: "1:539449615245:web:380fc44fa65e4f793953c4",
+    databaseURL: "https://futball-platform-prod-default-rtdb.asia-southeast1.firebasedatabase.app",
+    storageBucket: "futball-platform-prod.appspot.com",
     locationId: 'asia-south1',
-    apiKey: 'AIzaSyBEA_-pL-89zRoLNBCXdcHcFwZgsnZziK0',
-    authDomain: 'futball-platform-prod.firebaseapp.com',
-    messagingSenderId: '539449615245',
+    apiKey: "AIzaSyBEA_-pL-89zRoLNBCXdcHcFwZgsnZziK0",
+    authDomain: "futball-platform-prod.firebaseapp.com",
+    messagingSenderId: "539449615245",
   },
   urls: {
     admin: 'https://admin.ballzo.in',
     whatsAppGroup: 'https://chat.whatsapp.com/K0EJ1dZfG0d6n4PDhHdmRc',
   },
   locationApiKey: {
-    countryStateCityApiKey: 'Rjc1ZmpjdGhCbWRrV2JnMHdnUGptaklVZjNhVWFpMlpnRUJFV1VpbQ==', 
+    countryStateCityApiKey: 'Rjc1ZmpjdGhCbWRrV2JnMHdnUGptaklVZjNhVWFpMlpnRUJFV1VpbQ==',
   },
   production: true,
 };
