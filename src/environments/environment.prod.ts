6fe92d3e6e41c6f5c3bd6c5be2c674198f21f857
skullteek23 <pgoel681@gmail.com>
2023-11-17 19:17:39 +0530

    added locationID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     apiKey: "AIzaSyBN73nj_PDcbLazZF5EX67sxYua4oQ4IW8",
     authDomain: "football-platform-production.firebaseapp.com",
     projectId: "football-platform-production",
+    locationId: 'asia-south1',
     storageBucket: "football-platform-production.appspot.com",
     messagingSenderId: "767901644974",
     appId: "1:767901644974:web:0b580efb99a45bf345d53f"
