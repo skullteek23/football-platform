436f3f52f3aeeb5b9b82b018d1a99aa927b2bd2a
Harsh Singh Kaira <kairaharsh793@gmail.com>
2023-11-21 18:10:55 +0530

    BA-29: temprary checks

--- functions/src/checkUserExist.ts
+++ functions/src/checkUserExist.ts
@@ -1,4 +1,4 @@
-
+import {pluck} from "@monsterlessonsacademy/utilities";
 
 /**
  * Check if the user exists with the given phone number
@@ -6,7 +6,8 @@
  * @param {any} context
  * @return {Promise<UserRecord>}
  */
-export async function checkUserExist(data: any, context: any): Promise<any> {
-  let testVariable;
+export async function checkUserExist(
+  data: any, context: any): Promise<any> {
+  console.log(pluck([{name: "foo"}, {name: "bar"}], "name"));
   return true;
 }
