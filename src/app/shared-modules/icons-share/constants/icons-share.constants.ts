dd2c0feffb3d70f3f685115e17edb67ec10ebea3
skullteek23 <pgoel681@gmail.com>
2024-01-06 10:31:22 +0530

    added icon share description

--- src/app/shared-modules/icons-share/constants/icons-share.constants.ts
+++ src/app/shared-modules/icons-share/constants/icons-share.constants.ts
@@ -1,5 +1,9 @@
 export class IconsShareConstants {
   static readonly title = 'Ballzo Daily Games';
-  static readonly description = `Don't copy-paste. Use ballzo web app for pay-and-play games.`;
+  static readonly description = `Hi Guys! ⚽🌟
+I've booked my spot at Gallant Vaishali. Want to join me on the field? Bring your A-game and let's score some goals together!
+🥅🏃‍♂️ #Ballzo #PlayingFootballMadeEasy
+
+Use this link to join the game:`;
   static readonly shareButtonsInclude: string[] = ['whatsapp', 'facebook', 'copy', 'sms', 'email'];
 }
